f1b00123eb8536b42dd74430feb562f2f1dc26b4
awais-awiz <144072992+awais-awiz@users.noreply.github.com>
2026-02-08 23:12:22 +0500

    Delete src directory

--- src/components/customComponents/Copyright.jsx
+++
@@ -1,9 +0,0 @@
-import { cn } from "@/lib/utils";
-
-export const Copyright = ({ className }) => {
-    return (
-        <p className={cn("text-slate-500 m-0", className)}>
-            &copy; {new Date().getFullYear()} CipherLab. All rights reserved.
-        </p>
-    );
-};
